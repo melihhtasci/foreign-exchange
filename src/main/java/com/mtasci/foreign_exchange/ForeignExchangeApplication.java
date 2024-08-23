5bfdfc0aa7f0bff145b975aa751f7e375532d185
melihhtasci <melihhtasci@gmail.com>
2024-08-23 03:35:47 +0300

    application base exception and logging aspect added

---
+++ src/main/java/com/mtasci/foreign_exchange/ForeignExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.mtasci.foreign_exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignExchangeApplication.class, args);
+	}
+
+}
